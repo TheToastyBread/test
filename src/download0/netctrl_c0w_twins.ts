bb5c9fdf21c1bd6d0eb9b56dc19c85ac1ac789af
TheToastyBread <mitiorezakata@gmail.com>
2026-09-16 14:16:47 +0300

    Update netctrl_c0w_twins.ts

--- src/download0/netctrl_c0w_twins.ts
+++ src/download0/netctrl_c0w_twins.ts
@@ -648,8 +648,8 @@ function init () {
     return amaj === bmaj ? amin - bmin : amaj - bmaj
   }
 
-  if (compare_version(FW_VERSION, '9.00') < 0 || compare_version(FW_VERSION, '13.00') > 0) {
-    log('Unsupported PS4 firmware\nSupported: 9.00-13.00\nAborting...')
+  if (compare_version(FW_VERSION, '9.00') < 0 || compare_version(FW_VERSION, '13.52') > 0) {
+    log('Unsupported PS4 firmware\nSupported: 9.00-13.52\nAborting...')
     send_notification('Unsupported PS4 firmware\nAborting...')
     return false
   }
